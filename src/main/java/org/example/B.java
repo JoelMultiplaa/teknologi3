b049dee0160c41003f4b6da157c2e7da38e8f22d
JoelMultiplaa <joda0002@stud.kea.dk>
2025-01-07 18:30:51 +0100

    yml

--- src/main/java/org/example/B.java
+++ src/main/java/org/example/B.java
@@ -4,5 +4,6 @@ public class B {
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
